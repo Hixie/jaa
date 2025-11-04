ab9f1ed10e054321849761bffae1dfd596b8304e
Ian Hickson <ian@hixie.ch>
2025-11-04 13:55:01 -0800

    build script for macos

---
+++ app/build-macos.sh
@@ -0,0 +1,7 @@
+set -e
+flutter clean
+flutter build macos
+pushd build/macos/Build/Products/Release/
+zip -r jaa_macos.zip 'FIRST Tech Challenge Judge Advisor Assistant.app'
+popd
+echo build/macos/Build/Products/Release/jaa_macos.zip
